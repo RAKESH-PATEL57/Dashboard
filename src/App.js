bfd9cc64eaf37a5c5cb3d0c665d2210ebdd62865
Rakesh Patel <rakesh246808@gmail.com>
2023-06-23 21:29:13 +0530

    Switching of Dark mode and Light mode is done

--- src/App.js
+++ src/App.js
@@ -11,11 +11,11 @@ import { useStateContext } from './contexts/ContextProvider';
 import './App.css'
 
 const App = () => {
-    const { activeMenu, themeSettings, setThemeSettings } = useStateContext();
+    const { activeMenu, themeSettings, setThemeSettings, currentMode } = useStateContext();
 
   return (
 
-    <div>
+    <div className={currentMode === 'Dark' ? 'dark' : ''}>
 
         <BrowserRouter>
 
@@ -39,12 +39,14 @@ const App = () => {
                     </div>
                 )}
                 <div className={
-
-                    `dark:bg-main-bg bg-main-bg min-h-screen w-full ${ activeMenu ? 'md:ml-72'
+                    `dark:bg-main-dark-bg bg-main-bg min-h-screen w-full 
+                    ${ activeMenu 
+                        
+                    ? 'md:ml-72'
                     : 'flex-2'}`            
                 }>
                 
-                    <div className="fixed md:static bg-main-bg dark:bg:main-dark-bg navbar w-full">
+                    <div className="fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full">
                         <Navbar />
                     </div>
 
